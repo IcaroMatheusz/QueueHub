af0cd8006659995d4c1b68255976f200d7b0f7aa
Icaro Matheus <icaromatheus741@gmail.com>
2026-08-16 18:35:00 -0300

    chore: criação do projeto

---
+++ backend/teste.ts
@@ -0,0 +1,10 @@
+/* eslint-disable prettier/prettier */
+const titulo: string = "Computador não liga";
+
+const prioridade: number = 1;
+
+const resolvido: boolean = false;
+
+console.log(titulo);
+console.log(prioridade);
+console.log(resolvido);
